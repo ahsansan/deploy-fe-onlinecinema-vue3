5be2c802b60f6a1d2cc2c603c7d7360201087001
ahsansan <maulana.ahsan@ilcs.co.id>
2023-08-28 10:47:24 +0700

    perubahan config

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://be-online-cinema.onrender.com/api/cinema/v1/",
+  baseURL: "https://deploy-be-onlinecinema.vercel.app/api/cinema/v1/",
 });
 
 export const setAuthToken = (token) => {
